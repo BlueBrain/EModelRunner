8ee6ec21d150ee566810d3d0b0690ba5bfaf07c4
Anil Tuncel <anil.tuncel@epfl.ch>
2021-05-14 16:25:58 +0200

    use cmake in install_neuron.sh
    Change-Id: Ieab6b4edde36f615199dbe8cc0795a8e944e9f0d

--- .install_neuron.sh
+++ .install_neuron.sh
@@ -11,22 +11,20 @@ then
     echo 'Neuron was not fully installed in previous build, installing ...'
     mkdir -p ${SRC_DIR}
     cd ${SRC_DIR}
-
     if [ ! -d nrn ]
     then
         echo "Downloading NEURON from github ..."
-        git clone --depth 1 https://github.com/nrnhines/nrn.git >download.log 2>&1
+        git clone --depth 10 https://github.com/neuronsimulator/nrn.git  >download.log 2>&1
     else
-        echo "Neuron already downloaded"
+        echo "Neuron is already downloaded"
     fi
     cd nrn
-    
-    echo "Preparing NEURON ..."
-    ./build.sh >buildsh.log 2>&1
-    echo "Configuring NEURON ..."
-    PYTHON_BLD=${PYTHON_BIN} ./configure --prefix=${INSTALL_DIR} --without-x --with-nrnpython=${PYTHON_BIN} --disable-rx3d >configure.log 2>&1
+    echo "Building NEURON ..."
+    mkdir build
+    cd build
+    cmake .. -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} -DNRN_ENABLE_INTERVIEWS=OFF -DNRN_ENABLE_TESTS=OFF -DNRN_ENABLE_RX3D=OFF -DNRN_ENABLE_MPI=OFF
     echo "Installing NEURON ..."
-    make -j4 install >makeinstall.log 2>&1
+    make -j10 install >makeinstall.log 2>&1
 
     export PATH="${INSTALL_DIR}/x86_64/bin":${PATH}
     export PYTHONPATH="${INSTALL_DIR}/lib/python":${PYTHONPATH}
