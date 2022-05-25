eac53458a53bb1d1b937f25ea2d7cd83ee8a728a
Jaquier Aurélien Tristan <aurelien.jaquier@epfl.ch>
2022-05-25 16:08:57 +0200

    Add python3.10 support
    Change-Id: I5fffd00d14bfd2269b3183ac38b7c6a0e0c3f319

--- setup.py
+++ setup.py
@@ -64,6 +64,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Scientific/Engineering :: Bio-Informatics",
     ],
     include_package_data=True,
